4c809541fdedd78e10e7aee01e2d2aea0c28e74b
leitongchen <leitongchen93@gmail.com>
2021-04-13 20:00:51 +0200

    esercizio completato

--- myscript.js
+++ myscript.js
@@ -1,6 +1,83 @@
+var surnameList = ["Lannister", "Stark", "Tyrell", "Greyjoy", "Martell", "Bolton", "Tully", "Targaryen", "Baratheon", "Arryn", "Frey", "Tarly", "Mormont"];
+
+
+// Controllo dell'input dell'utente
+var inputDetected = false
+do {
+
+    var userSurname = prompt("Il cognome che inserirai verrà inserito nell'elenco delle più prestigiose casate di Westeros.").trim();
+
+    if (parseInt(userSurname)) {
+        alert("Il tuo input non è valido. Prego inserire solo caratteri dell'alfabeto.")
+    }
+    else if (userSurname !== null && userSurname !== "") {
+
+        surnameList.push(userSurname);
+        inputDetected = true;
+
+    }
+
+} while (!inputDetected);
+
+
+// trasformare i cognomi in uppercase
+var surnameListUppercase = [];
+
+for (var i = 0; i < surnameList.length; i++) {
+
+    surnameListUppercase.push(surnameList[i].toUpperCase());
+
+}
+
+
+// ordinare i cognomi in ordine alfabetico
+var alphabeticList = surnameListUppercase.sort();
+
+
+//Determinare la posizione del cognome inserito in userSurname 
+
+var listPosition = alphabeticList.indexOf(userSurname.toUpperCase());
+
+console.log("Il tuo cognome si trova alla posizione " + (listPosition + 1) + ".");
+console.log("Qui di seguito l'elenco dei cognomi in ordine alfabetico: " + alphabeticList.join(", "))
+
+//stampare l'elenco dei cognomi con la numerazione
+for (var j = 0; j < alphabeticList.length; j++) {
+
+    console.log((j + 1) + " " + alphabeticList[j]);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* var surnameList = ["Bianchi", "Neri", "Rossi", "Gialli", "Verdi", "Marroni", "Grigio"];
+
+
 var userSurname = prompt("Inserisci il tuo cognome").toLowerCase().trim();
 
-var surnameList = ["Bianchi", "Neri", "Rossi", "Gialli", "Verdi", "Marroni", "Grigio"];
 
 surnameList.push(userSurname);
 
@@ -12,53 +89,55 @@ for (var i = 0; i < surnameList.length; i++) {
     surnameListLower.push(surnameList[i].toLowerCase());
 }
 
-console.log(surnameListLower)
-console.log(surnameList)
+var alphabeticList = surnameListLower.sort();
+console.log("Ecco l'elenco dei cognomi in ordine alfabetico: " + alphabeticList.join(", "));
+
+*/
 
 
-// array che contiene i cognomi ordinati 
-var orderedList = []
+/*
+// array che contiene i cognomi ordinati
+ var orderedList = []
 
 var i = 0;
 
 // ciclo while per prendere in analisi un cognome alla volta
-while (i < surnameListLower.length) {
+ while (i < surnameListLower.length) {
 
     // variabile che contiene il cognome preso in visione
     var revSurname = surnameListLower[i]
 
 
-    // ciclo for per confrontare revSurname con listedSurname 
+    // ciclo for per confrontare revSurname con listedSurname
     // devo mettere a confronto il cognome preso in analisi "revSurname" con quelli già inseriti nell'array orderedList
-    
+
 
 
     var j = 0
-    do {
+    while (j <= orderedList.length) {
 
         //listedSurname prende i elementi che sono stati pushati dentro orderedList
-        var listedSurname = orderedList[j]; 
+        var listedSurname = orderedList[j];
 
         //mettere a confronto le stringhe dei cognomi:
-        // ovvero confronto tra revSurname e i cognomi già presenti in orderedList, 
+        // ovvero confronto tra revSurname e i cognomi già presenti in orderedList,
         // SE revSurname > listedSurname = true allora il cognome va pushato alla fine  array.push()
         // SE revSurname < listedSurname = true allora il cognome va pushato prima  array.unshift()
-        
+
         if (orderedList !== "" || revSurname > listedSurname) {
-            orderedList.push(listedSurname);
-            
+            orderedList.push(revSurname);
+
         } else if (revSurname < listedSurname) {
 
-            orderedList.unshift(listedSurname);
+            orderedList.unshift(revSurname);
 
         }
-    
-        j++
-    } while (j < orderedList.length)
 
-   
+        j++
+    }
+
 
     i++
 }
 
-console.log(orderedList);
+console.log(orderedList); */
